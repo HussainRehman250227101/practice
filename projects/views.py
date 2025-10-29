ad565c0732bffed0b2edba3ca2df38a10096ab3d
hussain <hussain@gmail.com>
2025-10-29 16:22:31 +0500

    added fixes

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 def all_projects(request):
-    return render(request, 'projects/all_projects.html')
+    
+    return render(request, 'projects/all_projects.html') 
